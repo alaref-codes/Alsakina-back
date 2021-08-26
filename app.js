8adeac5a7b6f43cb672fd2ff459ab7f460c09af4
alaref-codes <alarefabdo1@gmail.com>
2021-08-26 19:02:38 +0200

    Project structure

--- app.js
+++ app.js
@@ -2,18 +2,32 @@ const express       = require('express');
 const morgan        = require('morgan');
 const mongoose      = require('mongoose');
 const bodyParser    = require('body-parser');
+const webRoutes     = require('./routes/mainRoute')
 
 const app = express();
 
 const port = 3000;
 
-
-
 const dbURI = 'mongodb+srv://Alaref:2212000@cluster0.atouh.mongodb.net/subs?retryWrites=true&w=majority'
-mongoose.connect(dbURI , { useNewUrlParser: true ,  useUnifiedTopology: true , useCreateIndex: true })
-
+mongoose.connect(dbURI , { useNewUrlParser: true ,  useUnifiedTopology: true})
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(morgan('dev'))
+
+app.use((req,res , next) => {
+    console.log('---------- NEW REQUEST MADE ----------');
+    next();
+});
+
+
+app.set('view engine' , 'ejs');
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({extended: true}));
+
+app.use('/' , (req,res) => {
+    res.status(200).render('index')
+})
 
 module.exports = app;
--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app    = require('./app');
 const port = 3000;
 
 const server = http.createServer(app);
-const nei = http.createServer()
 server.listen(port , () => {
     console.log('listenining at port : ' , port);
 });
