6522f4ba1dd97f686a13573c500e2be9e8fbceed
alaref-codes <alarefabdo1@gmail.com>
2021-08-28 19:14:51 +0200

    Added the styling of the email form

--- models/subs.js
+++ models/subs.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const subSchema = new Schema({
+const subSchema = new Schema({          
     email: {
         type: String,
         match: /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
